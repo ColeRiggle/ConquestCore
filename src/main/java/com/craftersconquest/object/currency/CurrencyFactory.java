4f92e6d697e95ca29ba041de7f4a8545797f0f1c
coleriggle <Be7TheTaco23>
2020-06-14 13:52:56 -0400

    CurrencyFactory

---
+++ src/main/java/com/craftersconquest/object/currency/CurrencyFactory.java
@@ -0,0 +1,30 @@
+package com.craftersconquest.object.currency;
+
+import com.craftersconquest.core.ConquestCore;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
+public class CurrencyFactory {
+
+    private final ConquestCore instance;
+
+    private final Coins coins;
+
+    public CurrencyFactory(ConquestCore instance) {
+        this.instance = instance;
+        coins = new Coins(instance);
+    }
+
+    public Currency getCoinsCurrency() {
+        return coins;
+    }
+
+    public Currency getMaterialCurrency(Material material) {
+        return new MaterialCurrency(material);
+    }
+
+    public Currency getItemStackCurrency(ItemStack item) {
+        return new ItemStackCurrency(item);
+    }
+
+}
